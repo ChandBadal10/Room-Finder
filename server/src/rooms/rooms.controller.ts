3d30b24028d89596f30b665dd18495f8affd0cf9
Badal Chand <badalchand636@gmail.com>
2026-07-28 13:54:20 +0545

    Completed the Create Room getAllRooms and Get Single Room by Id

--- server/src/rooms/rooms.controller.ts
+++ server/src/rooms/rooms.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Req, UseGuards } from '@nestjs/common';
+import { RoomsService } from './rooms.service';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { UserRole } from 'src/users/schemas/user.schema';
+import { CreateRoomDto } from './dto/create-room.dto';
 
 @Controller('rooms')
-export class RoomsController {}
+export class RoomsController {
+    constructor(
+        private readonly roomService: RoomsService,
+    ) {}
+
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Roles(UserRole.OWNER)
+    @Post()
+    createRoom(
+        @Body() createRoomDto: CreateRoomDto,
+        @Req() request: any,
+    ) {
+        return this.roomService.createRoom(
+            createRoomDto,
+            request.user.id
+        )
+    }
+
+    @Get()
+    getAllRooms() {
+        return this.roomService.getAllRooms();
+    }
+
+    @Get(":id")
+    getRoomId(
+        @Param("id") id: string
+    ) {
+        return this.roomService.getRoomById(id);
+    }
+}
--- server/src/rooms/rooms.service.ts
+++ server/src/rooms/rooms.service.ts
@@ -1,4 +1,82 @@
-import { Injectable } from '@nestjs/common';
+import { BadGatewayException, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Room, RoomDocument } from './schemas/room.schema';
+import { Model } from 'mongoose';
+import { CreateRoomDto } from './dto/create-room.dto';
 
 @Injectable()
-export class RoomsService {}
+export class RoomsService {
+    constructor(
+        @InjectModel(Room.name)
+        private readonly roomModel: Model<RoomDocument>
+    ) {}
+
+    // Create Room
+    async createRoom(createRoomDto: CreateRoomDto, ownerId: string) {
+        try {
+            const room = new this.roomModel({
+                ...createRoomDto,
+                owner: ownerId
+            });
+
+            await room.save();
+
+            return {
+                success: true,
+                message: "Room created successfully",
+                room
+            }
+        } catch (error: any) {
+            return {
+                success: false,
+                message: "Internal Server Error",
+                error: error.message
+            }
+        }
+    }
+
+    //Get All Rooms
+    async getAllRooms() {
+        try {
+        const rooms = await this.roomModel.find().populate("owner", "fullName email phone");
+
+        return {
+            success: true,
+            message: "Rooms fetched successfully",
+            totalRooms: rooms.length,
+            rooms
+        };
+
+        } catch (error: any) {
+            return{
+                success: false,
+                message: "Internal Server Error",
+                error: error.message
+            }
+        }
+    }
+
+
+    //Get single Room
+    async getRoomById(id: string) {
+        try {
+            const room = await this.roomModel.findById(id).populate("owner", "fullName, phone, profileImage");
+
+            if(!room) {
+                throw new BadGatewayException("Room not found")
+            }
+
+            return {
+                success: true,
+                message: "Room fetched successfully",
+                room
+            }
+        } catch(error: any) {
+            return{
+                success: false,
+                message: "Internal Server Error",
+                error: error.message
+            }
+        }
+    }
+}
