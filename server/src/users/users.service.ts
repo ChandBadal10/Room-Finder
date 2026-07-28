cc6fd615eb166abb6d34c327cc42d1ccddc3cfff
Badal Chand <badalchand636@gmail.com>
2026-07-28 10:40:46 +0545

    evev

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User, UserDocument } from './schemas/user.schema';
+import { Model } from 'mongoose';
+import { CreateUserDto } from './dto/create-user.dto';
+
+
+
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(
+        @InjectModel(User.name)
+        private userModel: Model<UserDocument>,
+    ) {}
+
+
+    async create(createUserDto: CreateUserDto) {
+        const user = await this.userModel.create(createUserDto);
+        return user;
+    }
+
+    async findByEmail(email: string) {
+        return await this.userModel.findOne({email});
+    }
+
+    async findById(id: string) {
+        return await this.userModel.findById(id);
+    }
+
+    async findAll() {
+        return await this.userModel.find();
+    }
+
+}
